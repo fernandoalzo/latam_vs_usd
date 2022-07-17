56a544980ccf4713c7287e92105c44483418e39b
a26510 <fercho_1991_@hotmail.com>
2022-07-17 17:53:27 -0500

    otro

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
+info_paises = {
+    "colombia": { "moneda": "peso", "ISO": "COP" },
+    "mexico": { "moneda": "peso", "ISO": "MXN" },
+    "belice": { "moneda": "Dolar", "ISO": "BZD" },
+    "guatemala": { "moneda": "quetzal", "ISO": "GTQ" },
+    "salvador": { "moneda": "Dolar Gringo", "ISO": "USD" },
+    "honduras": { "moneda": "lempira", "ISO": "HNL" },
+    "nicaragua": { "moneda": "cordoba", "ISO": "NIO" },
+    "costa_rica": { "moneda": "Colon Costarricense", "ISO": "CRC" },
+    "panama": { "moneda": "balboa", "ISO": "PAB" },
+    "venezuela": { "moneda": "bolivar", "ISO": "VES" },
+    "guayanas": { "moneda": "dolar guayanes", "ISO": "GYD" },
+    "peru": { "moneda": "sol", "ISO": "PEN" },
+    "brazil": { "moneda": "real", "ISO": "BRL" },
+    "bolivia": { "moneda": "boliviano", "ISO": "BOB" },
+    "paraguay": { "moneda": "guarai", "ISO": "PYG" },
+    "chile": { "moneda": "peso", "ISO": "CLP" },
+    "argentina": { "moneda": "peso", "ISO": "ARS" },
+    "uruguay": { "moneda": "peso", "ISO": "UYU" }
+}
+
 const yahoofinanceapi = "XWdDhn1dp339zhlY2bwel69dTqdOjVeG65TcWxte"
 const headers_ = {
     "accept": "application/json",
@@ -63,10 +84,9 @@ function convert_timstamp_to_date_format(list_timestamp) {
     return lista_de_fechas
 }
 
-function crear_grafico(fechas, precios) {
-    console.log(fechas)
-    console.log(precios)
-    const $grafica_colombia = document.querySelector("#chart_colombia")
+function crear_grafico(fechas, precios, pais) {
+    // console.log(pais)
+    const $grafica = document.querySelector(`#chart_${pais}`)
     const labels = fechas
     const datos = {
         label: "Peso Colombiano Vs USD ultimos 90 dias",
@@ -74,7 +94,7 @@ function crear_grafico(fechas, precios) {
         borderColor: 'rgba(0, 0, 0, 1)',
         borderWidth: 5,
     }
-    new Chart($grafica_colombia, {
+    new Chart($grafica, {
         type: "line",
         data: {
             labels: labels,
@@ -119,14 +139,66 @@ function crear_grafico(fechas, precios) {
     })
 }
 
+function crear_pais(info_paises){
+    contenedor = document.querySelector("#contenedor")
+    for (pais in info_paises){
+        iso = info_paises[`${pais}`]["ISO"]
+        div1 = document.createElement("div")
+        div1.setAttribute("class", "col")
+
+        div2 = document.createElement("div")
+        div2.setAttribute("class", "card mb-4 rounded-3 shadow-sm")
+
+        div3 = document.createElement("div")
+        div3.setAttribute("class", "card-header py-3")
+
+        h4 = document.createElement("h4")
+        h4.setAttribute("class", "my-0 fw-normal")
+        nombre_pais = document.createTextNode(`${pais}`)        
+        h4.append(nombre_pais)
+        div3.append(h4)
+
+        div4 = document.createElement("div")
+        div4.setAttribute("class", "card-body")
+        canvas = document.createElement("canvas")
+        canvas.setAttribute("id", `chart_${pais}`)
+        canvas.style.ba
+        div4.append(canvas)
+        h1 = document.createElement("h1")
+        precio = document.createTextNode("$Precio")
+        h1.append(precio)
+        div4.append(h1)
+
+        link = document.createElement("a")
+        link.setAttribute("href", `https://finance.yahoo.com/quote/${iso}=X/`)
+        link.setAttribute("target", "_blank")
+        boton = document.createElement("button")
+        boton.setAttribute("type", "button")
+        boton.setAttribute("class", "w-100 btn btn-lg btn-outline-primary")
+        texto_boton = document.createTextNode("Mas informacion")
+        boton.append(texto_boton)        
+        link.append(boton)
+        div4.append(link)
+
+        div2.append(div3, div4)
+        div1.append(div2)
+        contenedor.append(div1)
+    }
+
+}
+
 async function main() {
     // const info_api_by_symbol = await get_instrument_info(endpoint_instrument_info)
     // const historical_price = await get_instrument_info(endpoint_historical_price)
     // const precio = get_info_price_by_symbol(info_api_by_symbol)
     // const historical_data = get_historical_price(historical_price)
-    const precios = [3800, 3900, 3950, 4000, 4100, 4200, 4150, 4050, 4000, 4600,]
     const fechas = ["label1", "", "label2", "label3", "label4", "label5", "label6", "label7", "label8", "label9", "label10",]
+    const precios = [3800, 3900, 3950, 4000, 4100, 4200, 4150, 4050, 4000, 4600,]    
     // crear_grafico(historical_data["fechas"], historical_data["precios"])
-    crear_grafico(fechas, precios)
+    // crear_grafico(fechas, precios)
+    crear_pais(info_paises)
+    for (pais in info_paises){
+        crear_grafico(fechas, precios, pais)
+    }
 }
 main()
