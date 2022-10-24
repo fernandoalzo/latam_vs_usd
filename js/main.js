f7d758f190a115d04da047ad50c8bffa24d37086
a26510 <fercho_1991_@hotmail.com>
2022-10-23 19:35:49 -0500

    otro

--- js/main.js
+++ js/main.js
@@ -1,38 +1,8 @@
-info_paises = {
-    "colombia": { "moneda": "peso", "ISO": "COP" },
-    "mexico": { "moneda": "peso", "ISO": "MXN" },
-    "belice": { "moneda": "Dolar", "ISO": "BZD" },
-    "guatemala": { "moneda": "quetzal", "ISO": "GTQ" },
-    "salvador": { "moneda": "Dolar Gringo", "ISO": "USD" },
-    "honduras": { "moneda": "lempira", "ISO": "HNL" },
-    "nicaragua": { "moneda": "cordoba", "ISO": "NIO" },
-    "costa_rica": { "moneda": "Colon Costarricense", "ISO": "CRC" },
-    "panama": { "moneda": "balboa", "ISO": "PAB" },
-    "venezuela": { "moneda": "bolivar", "ISO": "VES" },
-    "guayanas": { "moneda": "dolar guayanes", "ISO": "GYD" },
-    "peru": { "moneda": "sol", "ISO": "PEN" },
-    "brazil": { "moneda": "real", "ISO": "BRL" },
-    "bolivia": { "moneda": "boliviano", "ISO": "BOB" },
-    "paraguay": { "moneda": "guarai", "ISO": "PYG" },
-    "chile": { "moneda": "peso", "ISO": "CLP" },
-    "argentina": { "moneda": "peso", "ISO": "ARS" },
-    "uruguay": { "moneda": "peso", "ISO": "UYU" }
-}
-
-// const yahoofinanceapi = "PuE7Rqe48MnVsysW7mxI5CJupd4afqGL0cJqM8i0"
-// const yahoofinanceapi = "sdcyIni7kuaucmyJUJkYW55n6ilQL2306XYkXjPU"
-// const yahoofinanceapi = "IQBxSohM1T7AnrIu01RLP1RPlgacbJgT49ZeW0gn"
-const yahoofinanceapi = "RTcw3ezU187MyIakGTCdQ9mwXQijHdDQ27B4Mrc4"
-
-const headers_ = {
-    "accept": "application/json",
-    "X-API-KEY": yahoofinanceapi
-}
-
-async function get_instrument_info(endPoint_url) {
-    console.log("####################################")
-    console.log(endPoint_url)
-    console.log("####################################")
+async function get_instrument_info(endPoint_url, apiKey) {
+    const headers_ = {
+        "accept": "application/json",
+        "X-API-KEY": apiKey
+    }
     try {
         const response = await fetch(endPoint_url, {
             method: "GET",
@@ -43,7 +13,7 @@ async function get_instrument_info(endPoint_url) {
             return data
         } else if (response.status == 429) {
             console.log("Hacen falta licencias para funcionar")
-            $('#exampleModal').modal('show');
+            return 429
 
         } else {
             console.log("Ocurrrio un error realizando la consulta a la API")
@@ -67,7 +37,7 @@ async function get_instrument_info(endPoint_url) {
 //     return info_precio
 // }
 
-function get_historical_price(info_from_api) {    
+function get_historical_price(info_from_api) {
     const precios = info_from_api["chart"]["result"][0]["indicators"]["quote"][0]["close"]
     const timestamp = info_from_api["chart"]["result"][0]["timestamp"]
     const fechas = convert_timstamp_to_date_format(timestamp)
@@ -145,9 +115,9 @@ function crear_grafico(fechas, precios, pais, dias) {
     })
 }
 
-function crear_pais(info_paises){
+function crear_pais(info_paises) {
     contenedor = document.querySelector("#contenedor")
-    for (pais in info_paises){
+    for (pais in info_paises) {
         iso = info_paises[`${pais}`]["ISO"]
         div1 = document.createElement("div")
         div1.setAttribute("class", "col")
@@ -160,7 +130,7 @@ function crear_pais(info_paises){
 
         h4 = document.createElement("h4")
         h4.setAttribute("class", "my-0 fw-normal")
-        nombre_pais = document.createTextNode(`${pais}`)        
+        nombre_pais = document.createTextNode(`${pais}`)
         h4.append(nombre_pais)
         div3.append(h4)
 
@@ -173,7 +143,7 @@ function crear_pais(info_paises){
         canvas = document.createElement("canvas")
         canvas.setAttribute("id", `chart_${pais}`)
         canvas.style.ba
-        div4.append(canvas)       
+        div4.append(canvas)
 
 
         h3_apreciacion_value = document.createElement("h3")
@@ -188,7 +158,7 @@ function crear_pais(info_paises){
         boton.setAttribute("type", "button")
         boton.setAttribute("class", "w-100 btn btn-lg btn-outline-primary")
         texto_boton = document.createTextNode("Mas informacion")
-        boton.append(texto_boton)        
+        boton.append(texto_boton)
         link.append(boton)
         div4.append(link)
 
@@ -199,36 +169,76 @@ function crear_pais(info_paises){
 
 }
 async function main() {
+    info_paises = {
+        "colombia": { "moneda": "peso", "ISO": "COP" },
+        "mexico": { "moneda": "peso", "ISO": "MXN" },
+        "belice": { "moneda": "Dolar", "ISO": "BZD" },
+        "guatemala": { "moneda": "quetzal", "ISO": "GTQ" },
+        "salvador": { "moneda": "Dolar Gringo", "ISO": "USD" },
+        "honduras": { "moneda": "lempira", "ISO": "HNL" },
+        "nicaragua": { "moneda": "cordoba", "ISO": "NIO" },
+        "costa_rica": { "moneda": "Colon Costarricense", "ISO": "CRC" },
+        "panama": { "moneda": "balboa", "ISO": "PAB" },
+        "venezuela": { "moneda": "bolivar", "ISO": "VES" },
+        "guayanas": { "moneda": "dolar guayanes", "ISO": "GYD" },
+        "peru": { "moneda": "sol", "ISO": "PEN" },
+        "brazil": { "moneda": "real", "ISO": "BRL" },
+        "bolivia": { "moneda": "boliviano", "ISO": "BOB" },
+        "paraguay": { "moneda": "guarai", "ISO": "PYG" },
+        "chile": { "moneda": "peso", "ISO": "CLP" },
+        "argentina": { "moneda": "peso", "ISO": "ARS" },
+        "uruguay": { "moneda": "peso", "ISO": "UYU" }
+    }
+
+    const yahoofinanceapi = "PuE7Rqe48MnVsysW7mxI5CJupd4afqGL0cJqM8i0"
+    const yahoofinanceapi1 = "sdcyIni7kuaucmyJUJkYW55n6ilQL2306XYkXjPU"
+    const yahoofinanceapi2 = "IQBxSohM1T7AnrIu01RLP1RPlgacbJgT49ZeW0gn"
+    const yahoofinanceapi3 = "RTcw3ezU187MyIakGTCdQ9mwXQijHdDQ27B4Mrc4"
+
     crear_pais(info_paises)
-    for (pais in info_paises){
+
+    for (pais in info_paises) {
         iso_symbol = info_paises[`${pais}`]["ISO"]
         const dias = 60
-        // historical data
-        endpoint_historical_price = `https://yfapi.net/v8/finance/chart/USD${iso_symbol}%3DX?range=${dias}d&region=US&interval=1d&lang=en&events=div%2Csplit`
-        const historical_price = await get_instrument_info(endpoint_historical_price)
+        //endpoint historical data
+        const endpoint_historical_price = `https://yfapi.net/v8/finance/chart/USD${iso_symbol}%3DX?range=${dias}d&region=US&interval=1d&lang=en&events=div%2Csplit`
+        // get hispriacal data
+        const historical_price = await get_instrument_info(endpoint_historical_price, yahoofinanceapi)
+        if (historical_price === 429) {
+            const historical_price = await get_instrument_info(endpoint_historical_price, yahoofinanceapi1)
+            if (historical_price === 429) {
+                const historical_price = await get_instrument_info(endpoint_historical_price, yahoofinanceapi2)
+                if (historical_price === 429) {
+                    const historical_price = await get_instrument_info(endpoint_historical_price, yahoofinanceapi3)
+                    if (historical_price === 429) {
+                        $('#exampleModal').modal('show');
+                        break
+                    }
+                }
+            }
+        }
+        // if (historical_price[1] === 200) {
         const historical_data = get_historical_price(historical_price)
         // calcular la devauacion
         const precios = historical_data["precios"]
         const precio_inicial = Math.round(precios[0])
-        console.log(precio_inicial)
-        const precio_final = Math.round(precios[precios.length-1])
-        console.log(precio_final)
+        const precio_final = Math.round(precios[precios.length - 1])
         devaluacion = ((precio_inicial - precio_final) / precio_inicial) * 100
-        console.log(devaluacion)
         //set price into HTML
         // precio_actual = info_recio["precio"]
         precio_actual = precio_final
-        h1_precio = document.querySelector(`#precio_${pais}`)
-        h1_precio.append(document.createTextNode('Precio: ' + precio_actual))
+        h2_precio = document.querySelector(`#precio_${pais}`)
+        h2_precio.append(document.createTextNode('Precio: ' + precio_actual))
         //set apreciation into HTML
-        h1_precio = document.querySelector(`#apreciacion_price_${pais}`)
-        h1_precio.append(document.createTextNode(`${devaluacion.toFixed(1)}% `))
+        h3_devaluacion = document.querySelector(`#apreciacion_price_${pais}`)
+        h3_devaluacion.append(document.createTextNode(`${devaluacion.toFixed(1)}% `))
         //Crear graficos
         crear_grafico(historical_data["fechas"], historical_data["precios"], pais, dias)
+        // }
     }
 }
 main()
 
-window.onload = _=>{
-    console.log("mensaje para cada recarga")
-}
+// window.onload = _ => {
+//     console.log("mensaje para cada recarga")
+// }
